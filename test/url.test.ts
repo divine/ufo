8921c2380eafab2fc6b6fe5b7155d4dc40499b2b
divine <48183131+divine@users.noreply.github.com>
2023-12-13 21:51:20 +0000

    chore: improve url test

--- test/url.test.ts
+++ test/url.test.ts
@@ -1,57 +1,70 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
 import { $URL } from "../src";
 
 describe("$URL constructor", () => {
-  it("should throw a TypeError if input is not a string", () => {
-    expect(() => new $URL(undefined)).toThrow(TypeError);
-    expect(() => new $URL(undefined)).toThrow(TypeError);
-    expect(() => new $URL(123)).toThrow(TypeError);
-    expect(() => new $URL({})).toThrow(TypeError);
-  });
-  it("should parse a URL string correctly", () => {
-    const url = new $URL("https://example.com/path?query=value#hash");
-    expect(url.protocol).toBe("https:");
-    expect(url.host).toBe("example.com");
-    expect(url.pathname).toBe("/path");
-    expect(url.query).toEqual({ query: "value" });
-    expect(url.hash).toBe("hash");
-  });
+  const tests = [
+    // eslint-disable-next-line unicorn/no-null
+    {
+      input: null,
+      out: "TypeError: URL input should be string received object (null)",
+    },
+    {
+      input: 123,
+      out: "TypeError: URL input should be string received number (123)",
+    },
+    {
+      input: {},
+      out: "TypeError: URL input should be string received object ([object Object])",
+    },
+  ];
+
+  for (const t of tests) {
+    test(t.input.toString() + " throw", () => {
+      expect(new $URL(t.input)).toThrowError(t.out);
+    });
+  }
 });
+
 describe("$URL getters", () => {
-  it("should return the correct values", () => {
-    const url = new $URL("https://example.com/path?query=value#hash");
-    expect(url.hostname).toBe("example.com");
-    expect(url.port).toBe("");
-    expect(url.username).toBe("");
-    expect(url.password).toBe("");
-    expect(url.hasProtocol).toBe(true);
-    expect(url.isAbsolute).toBe(true);
-    expect(url.search).toBe("?query=value");
-    expect(url.searchParams.get("query")).toBe("value");
-    expect(url.origin).toBe("https://example.com");
-    expect(url.fullpath).toBe("/path?query=value#hash");
-    expect(url.encodedAuth).toBe("");
-    expect(url.href).toBe("https://example.com/path?query=value#hash");
-  });
+  const url = new $URL("https://example.com/path?query=value#hash");
+
+  const tests = [
+    { input: url.protocol, out: "https:" },
+    { input: url.host, out: "example.com" },
+    { input: url.pathname, out: "/path" },
+    { input: url.query, out: { query: "value" } },
+    { input: url.hash, out: "hash" },
+    { input: url.hostname, out: "example.com" },
+    { input: url.port, out: "" },
+    { input: url.username, out: "" },
+    { input: url.password, out: "" },
+    { input: url.hasProtocol, out: true },
+    { input: url.isAbsolute, out: true },
+    { input: url.search, out: "?query=value" },
+    { input: url.searchParams.get("query"), out: "value" },
+    { input: url.origin, out: "https://example.com" },
+    { input: url.fullpath, out: "/path?query=value#hash" },
+    { input: url.encodedAuth, out: "" },
+    { input: url.href, out: "https://example.com/path?query=value#hash" },
+    { input: url.toString(), out: "https://example.com/path?query=value#hash" },
+  ];
+
+  for (const t of tests) {
+    test(t.input.toString() + " to be " + JSON.stringify(t.out), () => {
+      expect(t.input).toBe(t.out);
+    });
+  }
 });
-describe("$URL append", () => {
-  it("should throw an error if the URL to append has a protocol", () => {
-    const url1 = new $URL("https://example.com/path");
-    const url2 = new $URL("http://example.com/path");
-    expect(() => url1.append(url2)).toThrow(Error);
-  });
-  it("should append the URL correctly", () => {
-    const url1 = new $URL("https://example.com/path?query=value#hash");
-    const url2 = new $URL("/newpath?newquery=newvalue#newhash");
-    url1.append(url2);
-    expect(url1.href).toBe(
-      "https://example.com/newpath?newquery=newvalue#newhash"
-    );
-  });
-});
-describe("$URL toString", () => {
-  it("should return the correct string representation", () => {
-    const url = new $URL("https://example.com/path?query=value#hash");
-    expect(url.toString()).toBe("https://example.com/path?query=value#hash");
-  });
+
+describe("$URL append with protocol", () => {
+  const url1 = new $URL("https://example.com/path");
+  const url2 = new $URL("http://example.com/path");
+  expect(url1.append(url2)).toThrowError("Cannot append a URL with protocol");
+
+  const url3 = new $URL("https://example.com/path?query=value#hash");
+  const url4 = new $URL("/newpath?newquery=newvalue#newhash");
+  url3.append(url4);
+  expect(url1.href).toBe(
+    "https://example.com/newpath?newquery=newvalue#newhash"
+  );
 });
