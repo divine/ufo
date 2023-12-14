cdcd28e213c0de1d351efff166782c4f77a78a6e
divine <48183131+divine@users.noreply.github.com>
2023-12-14 12:13:15 +0000

    fix:

--- test/url.test.ts
+++ test/url.test.ts
@@ -70,21 +70,57 @@ describe("$URL getters", () => {
 });
 
 describe("$URL append", () => {
-  const url = new $URL("https://example.com/path?query=value#hash");
+  const url = new $URL(
+    "https://john:doe@example.com:1080/path?query=value#hash"
+  );
   const url2 = new $URL("http://example.com/path");
   const path = new $URL("/newpath?newquery=newvalue#newhash");
 
-  test("append basic", () => {
-    expect(() =>
-      url
-        .append(path)
-        .toBe("https://example.com/newpath?newquery=newvalue#newhash")
-    );
-  });
+  url.append(path);
 
-  test("append with protocol", () => {
-    expect(() =>
-      url.append(url2).toThrow("Cannot append a URL with protocold")
-    );
-  });
+  const tests = [
+    { input: url.protocol, out: "https:" },
+    { input: url.host, out: "example.com:1080" },
+    { input: url.auth, out: "john:doe" },
+    { input: url.pathname, out: "/path/newpath" },
+    { input: url.query, out: { query: "value", newquery: "newvalue" } },
+    { input: url.hash, out: "#newhash" },
+    { input: url.hostname, out: "example.com" },
+    { input: url.port, out: "1080" },
+    { input: url.username, out: "john" },
+    { input: url.password, out: "doe" },
+    { input: url.hasProtocol, out: 6 },
+    { input: url.isAbsolute, out: 6 },
+    { input: url.search, out: "?query=value&newquery=newvalue" },
+    { input: url.searchParams.get("query"), out: "value" },
+    { input: url.searchParams.get("newquery"), out: "newvalue" },
+    { input: url.origin, out: "https://example.com:1080" },
+    {
+      input: url.fullpath,
+      out: "/path/newpath?query=value&newquery=newvalue#newhash",
+    },
+    { input: url.encodedAuth, out: "john:doe" },
+    {
+      input: url.href,
+      out: "https://john:doe@example.com:1080/path/newpath?query=value&newquery=newvalue#newhash",
+    },
+    {
+      input: url.toString(),
+      out: "https://john:doe@example.com:1080/path/newpath?query=value&newquery=newvalue#newhash",
+    },
+    {
+      input: url.toJSON(),
+      out: "https://john:doe@example.com:1080/path/newpath?query=value&newquery=newvalue#newhash",
+    },
+  ];
+
+  for (const t of tests) {
+    test(JSON.stringify(t.input) + " to be " + JSON.stringify(t.out), () => {
+      expect(t.input).toEqual(t.out);
+    });
+  }
+
+  /* test("with protocol", () => {
+    expect(url.append(url2).toThrow("Cannot append a URL with protocol"));
+  }); */
 });
